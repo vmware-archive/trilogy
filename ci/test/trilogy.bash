506f7bb4edd9dde87e6f2a257bfedb37022331df
Akshay Mankar <amankar@pivotal.io>
2017-05-26 09:46:36 +0100

    Assertion failure exit code test
    [#138559383]
    
    Signed-off-by: Konstantin Semenov <ksemenov@pivotal.io>

--- ci/test/trilogy.bash
+++ ci/test/trilogy.bash
@@ -10,13 +10,28 @@ projects() {
   echo -n "$(repo)/src/test/resources/projects"
 }
 
+test_cases() {
+  echo -n "$(repo)/src/test/resources/testcases"
+}
+
 run_project() {
   local project="$1"
   java -jar "$(trilogy)" --project="$(projects)/${project}" > /dev/null
-  echo $?
 }
+
+run_test_case() {
+  local test_case="$1"
+  java -jar "$(trilogy)" "$(test_cases)/${test_case}.stt" > /dev/null
+}
+
 T_OneTestFails() {
-  if [[ "$(run_project errors)" == "0" ]]; then
+  if run_project errors; then
+    $T_fail "Expected a non-zero code when at least one test has failed"
+  fi
+}
+
+T_OneAssertionFails() {
+  if run_test_case should_fail_with_sql; then
     $T_fail "Expected a non-zero code when at least one test has failed"
   fi
 }
